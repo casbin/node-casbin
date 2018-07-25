9260cd11fed3984a0a21374dc237a7a0dc651017
nodece <nodeces@gmail.com>
2018-07-25 14:54:58 +0800

    :pencil: Add code comments

--- src/effect/effector.ts
+++ src/effect/effector.ts
@@ -12,12 +12,15 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+// Values for policy effect.
 export enum Effect {
   Allow = 1,
   Indeterminate,
   Deny
 }
 
+// Effector is the interface for Casbin effectors.
 export interface Effector {
+  // mergeEffects merges all matching results collected by the enforcer into a single decision.
   mergeEffects(expr: string, effects: Effect[], results: number[]): boolean;
 }
