3a9c60af0ff67daa1548ddbf9204ee5e2f28bbf3
nodece <nodeces@gmail.com>
2018-07-26 17:12:00 +0800

    :art: Add export all for model folder

--- src/model/model.ts
+++ src/model/model.ts
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-import { Config } from './config';
+import { Config } from '../config';
 
 export class Model {
   private sectionNameMap: { [index: string]: string };
