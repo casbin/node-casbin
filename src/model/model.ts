ab6a5aebea09d492f0d71b33674e4827292b18fe
Chalin-Shi <18875906195@163.com>
2018-07-27 00:03:43 +0800

    :sparkles: add model ts file for model dir

--- src/model/model.ts
+++ src/model/model.ts
@@ -12,37 +12,111 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+import * as util from '../util';
 import { Config } from '../config';
+import { Assertion } from './assertion';
+
+const sectionNameMap: { [index: string]: string } = {
+  r: 'request_definition',
+  p: 'policy_definition',
+  g: 'role_definition',
+  e: 'policy_effect',
+  m: 'matchers'
+};
 
 export class Model {
-  private sectionNameMap: { [index: string]: string };
+  private model: Map<string, Map<string, Assertion>>;
 
   constructor() {
-    this.sectionNameMap = {};
+    this.model = new Map<string, Map<string, Assertion>>();
   }
 
-  private loadAssertion(
-    model: Model,
-    cfg: Config,
-    sec: string,
-    key: string
-  ): boolean {
-    return true;
-  }
-
-  public addDef(sec: string, key: string, value: string): boolean {
-    return true;
+  private loadAssertion(cfg: Config, sec: string, key: string): boolean {
+    const secName = sectionNameMap[sec];
+    const value = cfg.getString(`${secName}::${key}`);
+    return this.addDef(sec, key, value);
   }
 
   private getKeySuffix(i: number): string {
-    return '';
+    if (i === 1) {
+      return '';
+    }
+
+    return i.toString();
   }
 
-  private loadSection(model: Model, cfg: Config, sec: string): void {}
+  private loadSection(cfg: Config, sec: string): void {
+    let i = 1;
+    for (;;) {
+      if (!this.loadAssertion(cfg, sec, sec + this.getKeySuffix(i))) {
+        break;
+      } else {
+        i++;
+      }
+    }
+  }
 
-  public loadModel(path: string): void {}
+  public addDef(sec: string, key: string, value: string): boolean {
+    if (value === '') {
+      return false;
+    }
 
-  public loadModelFromText(text: string): void {}
+    const ast = new Assertion();
+    ast.key = key;
+    ast.value = value;
 
-  public printModel(): void {}
+    if (sec === 'r' || sec === 'p') {
+      const tokens = value.split(', ');
+      for (let i = 0; i < tokens.length; i++) {
+        tokens[i] = key + '_' + tokens[i];
+      }
+      ast.tokens = tokens;
+    } else {
+      ast.value = util.removeComments(util.escapeAssertion(value));
+    }
+
+    const assertionMap = new Map<string, Assertion>();
+    assertionMap.set(key, ast);
+
+    this.model.set(sec, assertionMap);
+    return true;
+  }
+
+  public loadModel(path: string): void {
+    const cfg = Config.newConfig(path);
+
+    this.loadSection(cfg, 'r');
+    this.loadSection(cfg, 'p');
+    this.loadSection(cfg, 'e');
+    this.loadSection(cfg, 'm');
+
+    this.loadSection(cfg, 'g');
+  }
+
+  public loadModelFromText(text: string): void {
+    const cfg = Config.newConfigFromText(text);
+
+    this.loadSection(cfg, 'r');
+    this.loadSection(cfg, 'p');
+    this.loadSection(cfg, 'e');
+    this.loadSection(cfg, 'm');
+
+    this.loadSection(cfg, 'g');
+  }
+
+  public printModel(): void {
+    util.logPrint('Model:');
+    for (const sec in this.model) {
+      if (this.model.hasOwnProperty(sec)) {
+        const astMap = this.model.get(sec) || new Map();
+        for (const key in astMap) {
+          if (astMap.hasOwnProperty(key)) {
+            const ast = astMap.get(key);
+            const value = ast ? ast.value : '';
+            util.logPrintf(`${sec}.${key}: ${value}`);
+          }
+        }
+      }
+    }
+  }
 }
