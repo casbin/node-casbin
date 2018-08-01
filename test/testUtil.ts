9282f0e107d073e03ac24f973e95447024c0bb97
nodece <nodeces@gmail.com>
2018-08-01 15:25:06 +0800

    Add test for Valuate class

--- test/testUtil.ts
+++ test/testUtil.ts
@@ -1,3 +1,12 @@
-test('add test', () => {
+import { Valuate } from '../src/util';
+
+test('jest', () => {
   console.log('hello jest');
+});
+
+test('Valuate', () => {
+  let ast = Valuate.parse('1 + 1 === 2');
+  expect(new Valuate().evaluate(ast)).toEqual(true);
+  ast = Valuate.parse('1 + 1 !== 2');
+  expect(new Valuate().evaluate(ast)).toEqual(false);
 });
