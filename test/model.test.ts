91c6a0b7cb37b3adfe2ca5ec3f2f066be08fbc7a
Yang Luo <hsluoyz@qq.com>
2018-08-04 20:51:55 +0800

    Add tests like "TestRBACModel" in model test.

--- test/model.test.ts
+++ test/model.test.ts
@@ -18,7 +18,11 @@ function testEnforce(e: Enforcer, sub: string, obj: string, act: string, res: bo
   expect(e.enforce(sub, obj, act)).toBe(res);
 }
 
-test('testBasicModel', () => {
+function testEnforceWithoutUsers(e: Enforcer, obj: string, act: string, res: boolean): void {
+  expect(e.enforce(obj, act)).toBe(res);
+}
+
+test('TestBasicModel', () => {
   const e = new Enforcer('examples/basic_model.conf', 'examples/basic_policy.csv');
 
   testEnforce(e, 'alice', 'data1', 'read', true);
@@ -31,7 +35,7 @@ test('testBasicModel', () => {
   testEnforce(e, 'bob', 'data2', 'write', true);
 });
 
-test('testBasicModelNoPolicy', () => {
+test('TestBasicModelNoPolicy', () => {
   const e = new Enforcer('examples/basic_model.conf');
 
   testEnforce(e, 'alice', 'data1', 'read', false);
@@ -42,4 +46,69 @@ test('testBasicModelNoPolicy', () => {
   testEnforce(e, 'bob', 'data1', 'write', false);
   testEnforce(e, 'bob', 'data2', 'read', false);
   testEnforce(e, 'bob', 'data2', 'write', false);
+});
+
+test('TestBasicModelWithRoot', () => {
+  const e = new Enforcer('examples/basic_with_root_model.conf', 'examples/basic_policy.csv');
+
+  testEnforce(e, 'alice', 'data1', 'read', true);
+  testEnforce(e, 'alice', 'data1', 'write', false);
+  testEnforce(e, 'alice', 'data2', 'read', false);
+  testEnforce(e, 'alice', 'data2', 'write', false);
+  testEnforce(e, 'bob', 'data1', 'read', false);
+  testEnforce(e, 'bob', 'data1', 'write', false);
+  testEnforce(e, 'bob', 'data2', 'read', false);
+  testEnforce(e, 'bob', 'data2', 'write', true);
+  testEnforce(e, 'root', 'data1', 'read', true);
+  testEnforce(e, 'root', 'data1', 'write', true);
+  testEnforce(e, 'root', 'data2', 'read', true);
+  testEnforce(e, 'root', 'data2', 'write', true);
+});
+
+test('TestBasicModelWithRootNoPolicy', () => {
+  const e = new Enforcer('examples/basic_with_root_model.conf');
+
+  testEnforce(e, 'alice', 'data1', 'read', false);
+  testEnforce(e, 'alice', 'data1', 'write', false);
+  testEnforce(e, 'alice', 'data2', 'read', false);
+  testEnforce(e, 'alice', 'data2', 'write', false);
+  testEnforce(e, 'bob', 'data1', 'read', false);
+  testEnforce(e, 'bob', 'data1', 'write', false);
+  testEnforce(e, 'bob', 'data2', 'read', false);
+  testEnforce(e, 'bob', 'data2', 'write', false);
+  testEnforce(e, 'root', 'data1', 'read', true);
+  testEnforce(e, 'root', 'data1', 'write', true);
+  testEnforce(e, 'root', 'data2', 'read', true);
+  testEnforce(e, 'root', 'data2', 'write', true);
+});
+
+test('TestBasicModelWithoutUsers', () => {
+  const e = new Enforcer('examples/basic_without_users_model.conf', 'examples/basic_without_users_policy.csv');
+
+  testEnforceWithoutUsers(e, 'data1', 'read', true);
+  testEnforceWithoutUsers(e, 'data1', 'write', false);
+  testEnforceWithoutUsers(e, 'data2', 'read', false);
+  testEnforceWithoutUsers(e, 'data2', 'write', true);
+});
+
+test('TestBasicModelWithoutResources', () => {
+  const e = new Enforcer('examples/basic_without_resources_model.conf', 'examples/basic_without_resources_policy.csv');
+
+  testEnforceWithoutUsers(e, 'alice', 'read', true);
+  testEnforceWithoutUsers(e, 'alice', 'write', false);
+  testEnforceWithoutUsers(e, 'bob', 'read', false);
+  testEnforceWithoutUsers(e, 'bob', 'write', true);
+});
+
+test('TestRBACModel', () => {
+  const e = new Enforcer('examples/rbac_model.conf', 'examples/rbac_policy.csv');
+
+  testEnforce(e, 'alice', 'data1', 'read', true);
+  testEnforce(e, 'alice', 'data1', 'write', false);
+  testEnforce(e, 'alice', 'data2', 'read', true);
+  testEnforce(e, 'alice', 'data2', 'write', true);
+  testEnforce(e, 'bob', 'data1', 'read', false);
+  testEnforce(e, 'bob', 'data1', 'write', false);
+  testEnforce(e, 'bob', 'data2', 'read', false);
+  testEnforce(e, 'bob', 'data2', 'write', true);
 });
