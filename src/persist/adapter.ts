86923411aecdd6fbdabe831b29e6b03f1a7a7852
Chalin-Shi <18875906195@163.com>
2018-08-10 21:58:04 +0800

    :rocket: all adapter method support async/await

--- src/persist/adapter.ts
+++ src/persist/adapter.ts
@@ -15,14 +15,14 @@
 import { Model } from '../model';
 
 export interface Adapter {
-  loadPolicy(model: Model): void;
+  loadPolicy(model: Model): Promise<void>;
 
-  savePolicy(model: Model): void;
+  savePolicy(model: Model): Promise<boolean>;
 
-  addPolicy(sec: string, ptype: string, rule: string[]): void;
+  addPolicy(sec: string, ptype: string, rule: string[]): Promise<void>;
 
-  removePolicy(sec: string, ptype: string, rule: string[]): void;
+  removePolicy(sec: string, ptype: string, rule: string[]): Promise<void>;
 
   removeFilteredPolicy(sec: string, ptype: string, fieldIndex: number, ...fieldValues: string[]
-  ): void;
+  ): Promise<void>;
 }
