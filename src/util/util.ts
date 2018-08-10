c97c72af60d371dc30b021611a6e30e6b06dd8a8
Chalin-Shi <18875906195@163.com>
2018-08-10 21:58:26 +0800

    :knife: add async read and write file function

--- src/util/util.ts
+++ src/util/util.ts
@@ -13,6 +13,7 @@
 // limitations under the License.
 
 import * as _ from 'lodash';
+import * as fs from 'fs';
 
 // escapeAssertion escapes the dots in the assertion,
 // because the expression evaluation doesn't support such variable names.
@@ -58,6 +59,30 @@ function setEquals(a: string[], b: string[]): boolean {
   return _.isEqual(_.sortedUniq(a), _.sortedUniq(b));
 }
 
+// readFile return a promise for readFile.
+function readFile(path: string, encoding?: string): any {
+  return new Promise((resolve, reject) => {
+    fs.readFile(path, encoding || 'utf8', (error, data) => {
+      if (error) {
+        reject(error);
+      }
+      resolve(data);
+    });
+  });
+}
+
+// writeFile return a promise for writeFile.
+function writeFile(path: string, file: string, encoding?: string): any {
+  return new Promise((resolve, reject) => {
+    fs.writeFile(path, file, encoding || 'utf8', (error, data) => {
+      if (error) {
+        reject(error);
+      }
+      resolve(data);
+    });
+  });
+}
+
 export {
   escapeAssertion,
   removeComments,
@@ -66,5 +91,7 @@ export {
   arrayRemoveDuplicates,
   arrayToString,
   paramsToString,
-  setEquals
+  setEquals,
+  readFile,
+  writeFile
 };
