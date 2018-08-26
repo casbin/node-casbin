056ddad197ccc6be61f928b6d504f5379a566374
Yang Luo <hsluoyz@qq.com>
2018-08-27 00:27:18 +0800

    Add all other enforcer tests.

--- test/enforcer.test.ts
+++ test/enforcer.test.ts
@@ -14,9 +14,17 @@
 
 import { Enforcer } from '../src/enforcer';
 import { FileAdapter } from '../src/persist';
+import { Util } from '../src/casbin';
 
 function testEnforce(e: Enforcer, sub: string, obj: string, act: string, res: boolean): void {
   expect(e.enforce(sub, obj, act)).toBe(res);
+}
+
+function testGetPolicy(e: Enforcer, res: string[][]) {
+  const myRes = e.getPolicy();
+  console.log('Policy: ', myRes);
+
+  expect(Util.array2DEquals(res, myRes)).toBe(true);
 }
 
 test('TestKeyMatchModelInMemory', async () => {
@@ -53,6 +61,7 @@ test('TestKeyMatchModelInMemory', async () => {
   testEnforce(e, 'cathy', '/cathy_data', 'DELETE', false);
 
   e = await Enforcer.newEnforcer(m);
+  a.loadPolicy(e.getModel());
 
   testEnforce(e, 'alice', '/alice_data/resource1', 'GET', true);
   testEnforce(e, 'alice', '/alice_data/resource1', 'POST', true);
@@ -89,4 +98,305 @@ test('TestKeyMatchModelInMemoryDeny', async () => {
   const e = await Enforcer.newEnforcer(m, a);
 
   testEnforce(e, 'alice', '/alice_data/resource2', 'POST', true);
+});
+
+test('TestRBACModelInMemoryIndeterminate', async () => {
+  const m = Enforcer.newModel();
+  m.addDef('r', 'r', 'sub, obj, act');
+  m.addDef('p', 'p', 'sub, obj, act');
+  m.addDef('g', 'g', '_, _');
+  m.addDef('e', 'e', 'some(where (p.eft == allow))');
+  m.addDef('m', 'm', 'g(r.sub, p.sub) && r.obj == p.obj && r.act == p.act');
+
+  const e = await Enforcer.newEnforcer(m);
+
+  e.addPermissionForUser('alice', 'data1', 'invalid');
+
+  testEnforce(e, 'alice', 'data1', 'read', false);
+});
+
+test('TestRBACModelInMemory', async () => {
+  const m = Enforcer.newModel();
+  m.addDef('r', 'r', 'sub, obj, act');
+  m.addDef('p', 'p', 'sub, obj, act');
+  m.addDef('g', 'g', '_, _');
+  m.addDef('e', 'e', 'some(where (p.eft == allow))');
+  m.addDef('m', 'm', 'g(r.sub, p.sub) && r.obj == p.obj && r.act == p.act');
+
+  const e = await Enforcer.newEnforcer(m);
+
+  e.addPermissionForUser('alice', 'data1', 'read');
+  e.addPermissionForUser('bob', 'data2', 'write');
+  e.addPermissionForUser('data2_admin', 'data2', 'read');
+  e.addPermissionForUser('data2_admin', 'data2', 'write');
+  e.addRoleForUser('alice', 'data2_admin');
+
+  testEnforce(e, 'alice', 'data1', 'read', true);
+  testEnforce(e, 'alice', 'data1', 'write', false);
+  testEnforce(e, 'alice', 'data2', 'read', true);
+  testEnforce(e, 'alice', 'data2', 'write', true);
+  testEnforce(e, 'bob', 'data1', 'read', false);
+  testEnforce(e, 'bob', 'data1', 'write', false);
+  testEnforce(e, 'bob', 'data2', 'read', false);
+  testEnforce(e, 'bob', 'data2', 'write', true);
+});
+
+test('TestRBACModelInMemory2', async () => {
+  const text =
+    `
+[request_definition]
+r = sub, obj, act
+
+[policy_definition]
+p = sub, obj, act
+
+[role_definition]
+g = _, _
+
+[policy_effect]
+e = some(where (p.eft == allow))
+
+[matchers]
+m = g(r.sub, p.sub) && r.obj == p.obj && r.act == p.act
+`;
+  const m = Enforcer.newModel(text);
+  // The above is the same as:
+  // const m = Enforcer.newModel();
+  // m.loadModelFromText(text);
+
+  const e = await Enforcer.newEnforcer(m);
+
+  e.addPermissionForUser('alice', 'data1', 'read');
+  e.addPermissionForUser('bob', 'data2', 'write');
+  e.addPermissionForUser('data2_admin', 'data2', 'read');
+  e.addPermissionForUser('data2_admin', 'data2', 'write');
+  e.addRoleForUser('alice', 'data2_admin');
+
+  testEnforce(e, 'alice', 'data1', 'read', true);
+  testEnforce(e, 'alice', 'data1', 'write', false);
+  testEnforce(e, 'alice', 'data2', 'read', true);
+  testEnforce(e, 'alice', 'data2', 'write', true);
+  testEnforce(e, 'bob', 'data1', 'read', false);
+  testEnforce(e, 'bob', 'data1', 'write', false);
+  testEnforce(e, 'bob', 'data2', 'read', false);
+  testEnforce(e, 'bob', 'data2', 'write', true);
+});
+
+test('TestNotUsedRBACModelInMemory', async () => {
+  const m = Enforcer.newModel();
+  m.addDef('r', 'r', 'sub, obj, act');
+  m.addDef('p', 'p', 'sub, obj, act');
+  m.addDef('g', 'g', '_, _');
+  m.addDef('e', 'e', 'some(where (p.eft == allow))');
+  m.addDef('m', 'm', 'g(r.sub, p.sub) && r.obj == p.obj && r.act == p.act');
+
+  const e = await Enforcer.newEnforcer(m);
+
+  e.addPermissionForUser('alice', 'data1', 'read');
+  e.addPermissionForUser('bob', 'data2', 'write');
+
+  testEnforce(e, 'alice', 'data1', 'read', true);
+  testEnforce(e, 'alice', 'data1', 'write', false);
+  testEnforce(e, 'alice', 'data2', 'read', false);
+  testEnforce(e, 'alice', 'data2', 'write', false);
+  testEnforce(e, 'bob', 'data1', 'read', false);
+  testEnforce(e, 'bob', 'data1', 'write', false);
+  testEnforce(e, 'bob', 'data2', 'read', false);
+  testEnforce(e, 'bob', 'data2', 'write', true);
+});
+
+test('TestMatcherUsingInOperator', async () => {
+// From file config
+  const e = await Enforcer.newEnforcer('examples/rbac_model_matcher_using_in_op.conf');
+  e.addPermissionForUser('alice', 'data1', 'read');
+
+  testEnforce(e, 'alice', 'data1', 'read', true);
+  testEnforce(e, 'alice', 'data2', 'read', true);
+  testEnforce(e, 'alice', 'data3', 'read', true);
+  testEnforce(e, 'anyone', 'data1', 'read', false);
+  testEnforce(e, 'anyone', 'data2', 'read', true);
+  testEnforce(e, 'anyone', 'data3', 'read', true);
+});
+
+test('TestReloadPolicy', async () => {
+  const e = await Enforcer.newEnforcer('examples/rbac_model.conf', 'examples/rbac_policy.csv');
+
+  e.loadPolicy();
+  testGetPolicy(e, [['alice', 'data1', 'read'], ['bob', 'data2', 'write'], ['data2_admin', 'data2', 'read'], ['data2_admin', 'data2', 'write']]);
+});
+
+test('TestSavePolicy', async () => {
+  const e = await Enforcer.newEnforcer('examples/rbac_model.conf', 'examples/rbac_policy.csv');
+
+  e.savePolicy();
+});
+
+test('TestClearPolicy', async () => {
+  const e = await Enforcer.newEnforcer('examples/rbac_model.conf', 'examples/rbac_policy.csv');
+
+  e.clearPolicy();
+});
+
+test('TestEnableEnforce', async () => {
+  const e = await Enforcer.newEnforcer('examples/basic_model.conf', 'examples/basic_policy.csv');
+
+  e.enableEnforce(false);
+  testEnforce(e, 'alice', 'data1', 'read', true);
+  testEnforce(e, 'alice', 'data1', 'write', true);
+  testEnforce(e, 'alice', 'data2', 'read', true);
+  testEnforce(e, 'alice', 'data2', 'write', true);
+  testEnforce(e, 'bob', 'data1', 'read', true);
+  testEnforce(e, 'bob', 'data1', 'write', true);
+  testEnforce(e, 'bob', 'data2', 'read', true);
+  testEnforce(e, 'bob', 'data2', 'write', true);
+
+  e.enableEnforce(true);
+  testEnforce(e, 'alice', 'data1', 'read', true);
+  testEnforce(e, 'alice', 'data1', 'write', false);
+  testEnforce(e, 'alice', 'data2', 'read', false);
+  testEnforce(e, 'alice', 'data2', 'write', false);
+  testEnforce(e, 'bob', 'data1', 'read', false);
+  testEnforce(e, 'bob', 'data1', 'write', false);
+  testEnforce(e, 'bob', 'data2', 'read', false);
+  testEnforce(e, 'bob', 'data2', 'write', true);
+});
+
+test('TestEnableLog', async () => {
+  const e = await Enforcer.newEnforcer('examples/basic_model.conf', 'examples/basic_policy.csv', true);
+  // The log is enabled by default, so the above is the same with:
+  // const e = await Enforcer.newEnforcer('examples/basic_model.conf', 'examples/basic_policy.csv');
+
+  testEnforce(e, 'alice', 'data1', 'read', true);
+  testEnforce(e, 'alice', 'data1', 'write', false);
+  testEnforce(e, 'alice', 'data2', 'read', false);
+  testEnforce(e, 'alice', 'data2', 'write', false);
+  testEnforce(e, 'bob', 'data1', 'read', false);
+  testEnforce(e, 'bob', 'data1', 'write', false);
+  testEnforce(e, 'bob', 'data2', 'read', false);
+  testEnforce(e, 'bob', 'data2', 'write', true);
+
+  // The log can also be enabled or disabled at run-time.
+  e.enableLog(false);
+  testEnforce(e, 'alice', 'data1', 'read', true);
+  testEnforce(e, 'alice', 'data1', 'write', false);
+  testEnforce(e, 'alice', 'data2', 'read', false);
+  testEnforce(e, 'alice', 'data2', 'write', false);
+  testEnforce(e, 'bob', 'data1', 'read', false);
+  testEnforce(e, 'bob', 'data1', 'write', false);
+  testEnforce(e, 'bob', 'data2', 'read', false);
+  testEnforce(e, 'bob', 'data2', 'write', true);
+});
+
+test('TestEnableAutoSave', async () => {
+  const e = await Enforcer.newEnforcer('examples/basic_model.conf', 'examples/basic_policy.csv');
+
+  e.enableAutoSave(false);
+  // Because AutoSave is disabled, the policy change only affects the policy in Casbin enforcer,
+  // it doesn't affect the policy in the storage.
+  e.removePolicy('alice', 'data1', 'read');
+  // Reload the policy from the storage to see the effect.
+  e.loadPolicy();
+  testEnforce(e, 'alice', 'data1', 'read', true);
+  testEnforce(e, 'alice', 'data1', 'write', false);
+  testEnforce(e, 'alice', 'data2', 'read', false);
+  testEnforce(e, 'alice', 'data2', 'write', false);
+  testEnforce(e, 'bob', 'data1', 'read', false);
+  testEnforce(e, 'bob', 'data1', 'write', false);
+  testEnforce(e, 'bob', 'data2', 'read', false);
+  testEnforce(e, 'bob', 'data2', 'write', true);
+
+  e.enableAutoSave(true);
+  // Because AutoSave is enabled, the policy change not only affects the policy in Casbin enforcer,
+  // but also affects the policy in the storage.
+  e.removePolicy('alice', 'data1', 'read');
+
+  // However, the file adapter doesn't implement the AutoSave feature, so enabling it has no effect at all here.
+
+  // Reload the policy from the storage to see the effect.
+  e.loadPolicy();
+  testEnforce(e, 'alice', 'data1', 'read', true); // Will not be false here.
+  testEnforce(e, 'alice', 'data1', 'write', false);
+  testEnforce(e, 'alice', 'data2', 'read', false);
+  testEnforce(e, 'alice', 'data2', 'write', false);
+  testEnforce(e, 'bob', 'data1', 'read', false);
+  testEnforce(e, 'bob', 'data1', 'write', false);
+  testEnforce(e, 'bob', 'data2', 'read', false);
+  testEnforce(e, 'bob', 'data2', 'write', true);
+});
+
+test('TestInitWithAdapter', async () => {
+  const adapter = new FileAdapter('examples/basic_policy.csv');
+  const e = await Enforcer.newEnforcer('examples/basic_model.conf', adapter);
+
+  testEnforce(e, 'alice', 'data1', 'read', true);
+  testEnforce(e, 'alice', 'data1', 'write', false);
+  testEnforce(e, 'alice', 'data2', 'read', false);
+  testEnforce(e, 'alice', 'data2', 'write', false);
+  testEnforce(e, 'bob', 'data1', 'read', false);
+  testEnforce(e, 'bob', 'data1', 'write', false);
+  testEnforce(e, 'bob', 'data2', 'read', false);
+  testEnforce(e, 'bob', 'data2', 'write', true);
+});
+
+test('TestRoleLinks', async () => {
+  const e = await Enforcer.newEnforcer('examples/rbac_model.conf');
+  e.enableAutoBuildRoleLinks(false);
+  e.buildRoleLinks();
+  e.enforce('user501', 'data9', 'read');
+});
+
+test('TestGetAndSetModel', async () => {
+  const e = await Enforcer.newEnforcer('examples/basic_model.conf', 'examples/basic_policy.csv');
+  const e2 = await Enforcer.newEnforcer('examples/basic_with_root_model.conf', 'examples/basic_policy.csv');
+
+  testEnforce(e, 'root', 'data1', 'read', false);
+
+  e.setModel(e2.getModel());
+
+  testEnforce(e, 'root', 'data1', 'read', true);
+});
+
+test('TestGetAndSetAdapterInMem', async () => {
+  const e = await Enforcer.newEnforcer('examples/basic_model.conf', 'examples/basic_policy.csv');
+  const e2 = await Enforcer.newEnforcer('examples/basic_model.conf', 'examples/basic_inverse_policy.csv');
+
+  testEnforce(e, 'alice', 'data1', 'read', true);
+  testEnforce(e, 'alice', 'data1', 'write', false);
+
+  const a2 = e2.getAdapter();
+  e.setAdapter(a2);
+  e.loadPolicy();
+
+  testEnforce(e, 'alice', 'data1', 'read', false);
+  testEnforce(e, 'alice', 'data1', 'write', true);
+});
+
+test('TestSetAdapterFromFile', async () => {
+  const e = await Enforcer.newEnforcer('examples/basic_model.conf');
+
+  testEnforce(e, 'alice', 'data1', 'read', false);
+
+  const a = new FileAdapter('examples/basic_policy.csv');
+  e.setAdapter(a);
+  e.loadPolicy();
+
+  testEnforce(e, 'alice', 'data1', 'read', true);
+});
+
+test('TestInitEmpty', async () => {
+  const e = await Enforcer.newEnforcer();
+
+  const m = Enforcer.newModel();
+  m.addDef('r', 'r', 'sub, obj, act');
+  m.addDef('p', 'p', 'sub, obj, act');
+  m.addDef('e', 'e', 'some(where (p.eft == allow))');
+  m.addDef('m', 'm', 'r.sub == p.sub && keyMatch(r.obj, p.obj) && regexMatch(r.act, p.act)');
+
+  const a = new FileAdapter('examples/keymatch_policy.csv');
+
+  e.setModel(m);
+  e.setAdapter(a);
+  e.loadPolicy();
+
+  testEnforce(e, 'alice', '/alice_data/resource1', 'GET', true);
 });
