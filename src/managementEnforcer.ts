d655ad5ebfc333b389ba3bd3d2f5994f333391e6
Yang Luo <hsluoyz@qq.com>
2018-08-27 13:51:52 +0800

    Refactor out ManagementEnforcer from Enforcer class.

---
+++ src/managementEnforcer.ts
@@ -0,0 +1,413 @@
+// Copyright 2018 The Casbin Authors. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import { InternalEnforcer } from './internalEnforcer';
+
+/**
+ * ManagementEnforcer = InternalEnforcer + Management API.
+ */
+export class ManagementEnforcer extends InternalEnforcer {
+  /**
+   * getAllSubjects gets the list of subjects that show up in the current policy.
+   *
+   * @return all the subjects in "p" policy rules. It actually collects the
+   *         0-index elements of "p" policy rules. So make sure your subject
+   *         is the 0-index element, like (sub, obj, act). Duplicates are removed.
+   */
+  public getAllSubjects(): string[] {
+    return this.getAllNamedSubjects('p');
+  }
+
+  /**
+   * getAllNamedSubjects gets the list of subjects that show up in the currentnamed policy.
+   *
+   * @param ptype the policy type, can be "p", "p2", "p3", ..
+   * @return all the subjects in policy rules of the ptype type. It actually
+   *         collects the 0-index elements of the policy rules. So make sure
+   *         your subject is the 0-index element, like (sub, obj, act).
+   *         Duplicates are removed.
+   */
+  public getAllNamedSubjects(ptype: string): string[] {
+    return this.model.getValuesForFieldInPolicy('p', ptype, 0);
+  }
+
+  /**
+   * getAllObjects gets the list of objects that show up in the current policy.
+   *
+   * @return all the objects in "p" policy rules. It actually collects the
+   *         1-index elements of "p" policy rules. So make sure your object
+   *         is the 1-index element, like (sub, obj, act).
+   *         Duplicates are removed.
+   */
+  public getAllObjects(): string[] {
+    return this.getAllNamedObjects('p');
+  }
+
+  /**
+   * getAllNamedObjects gets the list of objects that show up in the current named policy.
+   *
+   * @param ptype the policy type, can be "p", "p2", "p3", ..
+   * @return all the objects in policy rules of the ptype type. It actually
+   *         collects the 1-index elements of the policy rules. So make sure
+   *         your object is the 1-index element, like (sub, obj, act).
+   *         Duplicates are removed.
+   */
+  public getAllNamedObjects(ptype: string): string[] {
+    return this.model.getValuesForFieldInPolicy('p', ptype, 1);
+  }
+
+  /**
+   * getAllActions gets the list of actions that show up in the current policy.
+   *
+   * @return all the actions in "p" policy rules. It actually collects
+   *         the 2-index elements of "p" policy rules. So make sure your action
+   *         is the 2-index element, like (sub, obj, act).
+   *         Duplicates are removed.
+   */
+  public getAllActions(): string[] {
+    return this.getAllNamedActions('p');
+  }
+
+  /**
+   * GetAllNamedActions gets the list of actions that show up in the current named policy.
+   *
+   * @param ptype the policy type, can be "p", "p2", "p3", ..
+   * @return all the actions in policy rules of the ptype type. It actually
+   *         collects the 2-index elements of the policy rules. So make sure
+   *         your action is the 2-index element, like (sub, obj, act).
+   *         Duplicates are removed.
+   */
+  public getAllNamedActions(ptype: string): string[] {
+    return this.model.getValuesForFieldInPolicy('p', ptype, 2);
+  }
+
+  /**
+   * getAllRoles gets the list of roles that show up in the current policy.
+   *
+   * @return all the roles in "g" policy rules. It actually collects
+   *         the 1-index elements of "g" policy rules. So make sure your
+   *         role is the 1-index element, like (sub, role).
+   *         Duplicates are removed.
+   */
+  public getAllRoles(): string[] {
+    return this.getAllNamedRoles('g');
+  }
+
+  /**
+   * getAllNamedRoles gets the list of roles that show up in the current named policy.
+   *
+   * @param ptype the policy type, can be "g", "g2", "g3", ..
+   * @return all the subjects in policy rules of the ptype type. It actually
+   *         collects the 0-index elements of the policy rules. So make
+   *         sure your subject is the 0-index element, like (sub, obj, act).
+   *         Duplicates are removed.
+   */
+  public getAllNamedRoles(ptype: string): string[] {
+    return this.model.getValuesForFieldInPolicy('g', ptype, 1);
+  }
+
+  /**
+   * getPolicy gets all the authorization rules in the policy.
+   *
+   * @return all the "p" policy rules.
+   */
+  public getPolicy(): string[][] {
+    return this.getNamedPolicy('p');
+  }
+
+  /**
+   * getFilteredPolicy gets all the authorization rules in the policy, field filters can be specified.
+   *
+   * @param fieldIndex the policy rule's start index to be matched.
+   * @param fieldValues the field values to be matched, value ""
+   *                    means not to match this field.
+   * @return the filtered "p" policy rules.
+   */
+  public getFilteredPolicy(fieldIndex: number, ...fieldValues: string[]): string[][] {
+    return this.getFilteredNamedPolicy('p', fieldIndex, ...fieldValues);
+  }
+
+  /**
+   * getNamedPolicy gets all the authorization rules in the named policy.
+   *
+   * @param ptype the policy type, can be "p", "p2", "p3", ..
+   * @return the "p" policy rules of the specified ptype.
+   */
+  public getNamedPolicy(ptype: string): string[][] {
+    return this.model.getPolicy('p', ptype);
+  }
+
+  /**
+   * getFilteredNamedPolicy gets all the authorization rules in the named policy, field filters can be specified.
+   *
+   * @param ptype the policy type, can be "p", "p2", "p3", ..
+   * @param fieldIndex the policy rule's start index to be matched.
+   * @param fieldValues the field values to be matched, value ""
+   *                    means not to match this field.
+   * @return the filtered "p" policy rules of the specified ptype.
+   */
+  public getFilteredNamedPolicy(ptype: string, fieldIndex: number, ...fieldValues: string[]): string[][] {
+    return this.model.getFilteredPolicy('p', ptype, fieldIndex, ...fieldValues);
+  }
+
+  /**
+   * getGroupingPolicy gets all the role inheritance rules in the policy.
+   *
+   * @return all the "g" policy rules.
+   */
+  public getGroupingPolicy(): string[][] {
+    return this.getNamedGroupingPolicy('g');
+  }
+
+  /**
+   * getFilteredGroupingPolicy gets all the role inheritance rules in the policy, field filters can be specified.
+   *
+   * @param fieldIndex the policy rule's start index to be matched.
+   * @param fieldValues the field values to be matched, value "" means not to match this field.
+   * @return the filtered "g" policy rules.
+   */
+  public getFilteredGroupingPolicy(fieldIndex: number, ...fieldValues: string[]): string[][] {
+    return this.getFilteredNamedGroupingPolicy('g', fieldIndex, ...fieldValues);
+  }
+
+  /**
+   * getNamedGroupingPolicy gets all the role inheritance rules in the policy.
+   *
+   * @param ptype the policy type, can be "g", "g2", "g3", ..
+   * @return the "g" policy rules of the specified ptype.
+   */
+  public getNamedGroupingPolicy(ptype: string): string[][] {
+    return this.model.getPolicy('g', ptype);
+  }
+
+  /**
+   * getFilteredNamedGroupingPolicy gets all the role inheritance rules in the policy, field filters can be specified.
+   *
+   * @param ptype the policy type, can be "g", "g2", "g3", ..
+   * @param fieldIndex the policy rule's start index to be matched.
+   * @param fieldValues the field values to be matched, value ""
+   *                    means not to match this field.
+   * @return the filtered "g" policy rules of the specified ptype.
+   */
+  public getFilteredNamedGroupingPolicy(ptype: string, fieldIndex: number, ...fieldValues: string[]): string[][] {
+    return this.model.getFilteredPolicy('g', ptype, fieldIndex, ...fieldValues);
+  }
+
+  /**
+   * hasPolicy determines whether an authorization rule exists.
+   *
+   * @param params the "p" policy rule, ptype "p" is implicitly used.
+   * @return whether the rule exists.
+   */
+  public hasPolicy(...params: string[]): boolean {
+    return this.hasNamedPolicy('p', ...params);
+  }
+
+  /**
+   * hasNamedPolicy determines whether a named authorization rule exists.
+   *
+   * @param ptype the policy type, can be "p", "p2", "p3", ..
+   * @param params the "p" policy rule.
+   * @return whether the rule exists.
+   */
+  public hasNamedPolicy(ptype: string, ...params: string[]): boolean {
+    return this.model.hasPolicy('p', ptype, params);
+  }
+
+  /**
+   * addPolicy adds an authorization rule to the current policy.
+   * If the rule already exists, the function returns false and the rule will not be added.
+   * Otherwise the function returns true by adding the new rule.
+   *
+   * @param params the "p" policy rule, ptype "p" is implicitly used.
+   * @return succeeds or not.
+   */
+  public async addPolicy(...params: string[]): Promise<boolean> {
+    return this.addNamedPolicy('p', ...params);
+  }
+
+  /**
+   * addNamedPolicy adds an authorization rule to the current named policy.
+   * If the rule already exists, the function returns false and the rule will not be added.
+   * Otherwise the function returns true by adding the new rule.
+   *
+   * @param ptype the policy type, can be "p", "p2", "p3", ..
+   * @param params the "p" policy rule.
+   * @return succeeds or not.
+   */
+  public async addNamedPolicy(ptype: string, ...params: string[]): Promise<boolean> {
+    return await this.addPolicyInternal('p', ptype, params);
+  }
+
+  /**
+   * removePolicy removes an authorization rule from the current policy.
+   *
+   * @param params the "p" policy rule, ptype "p" is implicitly used.
+   * @return succeeds or not.
+   */
+  public async removePolicy(...params: string[]): Promise<boolean> {
+    return await this.removeNamedPolicy('p', ...params);
+  }
+
+  /**
+   * removeFilteredPolicy removes an authorization rule from the current policy, field filters can be specified.
+   *
+   * @param fieldIndex the policy rule's start index to be matched.
+   * @param fieldValues the field values to be matched, value ""
+   *                    means not to match this field.
+   * @return succeeds or not.
+   */
+  public async removeFilteredPolicy(fieldIndex: number, ...fieldValues: string[]): Promise<boolean> {
+    return await this.removeFilteredNamedPolicy('p', fieldIndex, fieldValues);
+  }
+
+  /**
+   * removeNamedPolicy removes an authorization rule from the current named policy.
+   *
+   * @param ptype the policy type, can be "p", "p2", "p3", ..
+   * @param params the "p" policy rule.
+   * @return succeeds or not.
+   */
+  public async removeNamedPolicy(ptype: string, ...params: string[]): Promise<boolean> {
+    return await this.removePolicyInternal('p', ptype, params);
+  }
+
+  /**
+   * removeFilteredNamedPolicy removes an authorization rule from the current named policy, field filters can be specified.
+   *
+   * @param ptype the policy type, can be "p", "p2", "p3", ..
+   * @param fieldIndex the policy rule's start index to be matched.
+   * @param fieldValues the field values to be matched, value ""
+   *                    means not to match this field.
+   * @return succeeds or not.
+   */
+  public async removeFilteredNamedPolicy(ptype: string, fieldIndex: number, fieldValues: string[]): Promise<boolean> {
+    return await this.removeFilteredPolicyInternal('p', ptype, fieldIndex, fieldValues);
+  }
+
+  /**
+   * hasGroupingPolicy determines whether a role inheritance rule exists.
+   *
+   * @param params the "g" policy rule, ptype "g" is implicitly used.
+   * @return whether the rule exists.
+   */  public hasGroupingPolicy(...params: string[]): boolean {
+    return this.hasNamedGroupingPolicy('g', ...params);
+  }
+
+  /**
+   * hasNamedGroupingPolicy determines whether a named role inheritance rule exists.
+   *
+   * @param ptype the policy type, can be "g", "g2", "g3", ..
+   * @param params the "g" policy rule.
+   * @return whether the rule exists.
+   */
+  public hasNamedGroupingPolicy(ptype: string, ...params: string[]): boolean {
+    return this.model.hasPolicy('g', ptype, params);
+  }
+
+  /**
+   * addGroupingPolicy adds a role inheritance rule to the current policy.
+   * If the rule already exists, the function returns false and the rule will not be added.
+   * Otherwise the function returns true by adding the new rule.
+   *
+   * @param params the "g" policy rule, ptype "g" is implicitly used.
+   * @return succeeds or not.
+   */
+  public async addGroupingPolicy(...params: string[]): Promise<boolean> {
+    return await this.addNamedGroupingPolicy('g', ...params);
+  }
+
+  /**
+   * addNamedGroupingPolicy adds a named role inheritance rule to the current policy.
+   * If the rule already exists, the function returns false and the rule will not be added.
+   * Otherwise the function returns true by adding the new rule.
+   *
+   * @param ptype the policy type, can be "g", "g2", "g3", ..
+   * @param params the "g" policy rule.
+   * @return succeeds or not.
+   */
+  public async addNamedGroupingPolicy(ptype: string, ...params: string[]): Promise<boolean> {
+    const ruleadded = await this.addPolicyInternal('g', ptype, params);
+
+    if (this.autoBuildRoleLinks) {
+      this.buildRoleLinks();
+    }
+
+    return ruleadded;
+  }
+
+  /**
+   * removeGroupingPolicy removes a role inheritance rule from the current policy.
+   *
+   * @param params the "g" policy rule, ptype "g" is implicitly used.
+   * @return succeeds or not.
+   */
+  public async removeGroupingPolicy(...params: string[]): Promise<boolean> {
+    return await this.removeNamedGroupingPolicy('g', ...params);
+  }
+
+  /**
+   * removeFilteredGroupingPolicy removes a role inheritance rule from the current policy, field filters can be specified.
+   *
+   * @param fieldIndex the policy rule's start index to be matched.
+   * @param fieldValues the field values to be matched, value ""
+   *                    means not to match this field.
+   * @return succeeds or not.
+   */
+  public async removeFilteredGroupingPolicy(fieldIndex: number, ...fieldValues: string[]): Promise<boolean> {
+    return await this.removeFilteredNamedGroupingPolicy('g', fieldIndex, ...fieldValues);
+  }
+
+  /**
+   * removeNamedGroupingPolicy removes a role inheritance rule from the current named policy.
+   *
+   * @param ptype the policy type, can be "g", "g2", "g3", ..
+   * @param params the "g" policy rule.
+   * @return succeeds or not.
+   */
+  public async removeNamedGroupingPolicy(ptype: string, ...params: string[]): Promise<boolean> {
+    const ruleRemoved = await this.removePolicy('g', ptype, ...params);
+
+    if (this.autoBuildRoleLinks) {
+      this.buildRoleLinks();
+    }
+    return ruleRemoved;
+  }
+
+  /**
+   * removeFilteredNamedGroupingPolicy removes a role inheritance rule from the current named policy, field filters can be specified.
+   *
+   * @param ptype the policy type, can be "g", "g2", "g3", ..
+   * @param fieldIndex the policy rule's start index to be matched.
+   * @param fieldValues the field values to be matched, value ""
+   *                    means not to match this field.
+   * @return succeeds or not.
+   */
+  public async removeFilteredNamedGroupingPolicy(ptype: string, fieldIndex: number, ...fieldValues: string[]): Promise<boolean> {
+    const ruleRemoved = await this.removeFilteredPolicyInternal('g', ptype, fieldIndex, fieldValues);
+    if (this.autoBuildRoleLinks) {
+      this.buildRoleLinks();
+    }
+    return ruleRemoved;
+  }
+
+  /**
+   * addFunction adds a customized function.
+   * @param name custom function name
+   * @param func function
+   */
+  public addFunction(name: string, func: any): void {
+    this.fm.addFunction(name, func);
+  }
+}
