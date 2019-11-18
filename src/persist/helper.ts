687c10f01d35c30a7e88bc2deef18187f0396dd7
Zixuan Liu <nodeces@gmail.com>
2019-11-18 22:46:57 +0800

    Fix split error

--- src/persist/helper.ts
+++ src/persist/helper.ts
@@ -2,11 +2,11 @@ import { Model } from '../model';
 
 export class Helper {
   public static loadPolicyLine(line: string, model: Model): void {
-    if (line === '' || line.charAt(0) === '#') {
+    if (!line || line.trim() === '' || line.charAt(0) === '#') {
       return;
     }
 
-    const tokens = line.split(', ').map(n => n.trim());
+    const tokens = line.split(',').map(n => n.trim());
     const key = tokens[0];
     const sec = key.substring(0, 1);
     const item = model.model.get(sec);
