ce3d735a9341f695ccff9ef97c96d8c096893b53
DivyPatel9881 <divy9881@gmail.com>
2020-04-26 16:12:59 +0530

    Added batch adapter interface.

---
+++ src/persist/batchAdapter.ts
@@ -0,0 +1,11 @@
+import { Adapter } from './adapter'
+
+// BatchAdapter is the interface for Casbin adapters with multiple add and remove policy functions.
+export interface BatchAdapter extends Adapter{
+	// AddPolicies adds policy rules to the storage.
+	// This is part of the Auto-Save feature.
+	AddPolicies(sec: string, ptype: string, rules: string[][]): Promise<void>;
+	// RemovePolicies removes policy rules from the storage.
+	// This is part of the Auto-Save feature.
+	RemovePolicies(sec: string, ptype: string, rules: string[][]): Promise<void>;
+}
