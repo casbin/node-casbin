aa7271de847fdb5ff5b97f44ce0a00ca056d7f28
木杉 <zhmushan@qq.com>
2020-08-08 22:33:19 +0800

    chore: remove unused import (#178)
    Signed-off-by: 木杉 <zhmushan@qq.com>

--- src/config.ts
+++ src/config.ts
@@ -12,7 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 import { readFileSync } from 'fs';
-import { removeComments } from './util';
 
 // ConfigInterface defines the behavior of a Config implementation
 export interface ConfigInterface {
