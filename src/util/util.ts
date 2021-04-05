1960adbc376aebb86c0d6576e395981a8e2a8d09
Zxilly <zhouxinyu1001@gmail.com>
2021-04-05 16:01:31 +0800

    feat: add deepCopy function
    Signed-off-by: Zxilly <zhouxinyu1001@gmail.com>

--- src/util/util.ts
+++ src/util/util.ts
@@ -159,6 +159,21 @@ async function generatorRunAsync(iterator: Generator<any>): Promise<any> {
   }
 }
 
+function deepCopy(obj: Array<any> | any): any {
+  // 只拷贝对象
+  if (typeof obj !== 'object') return;
+  // 根据obj的类型判断是新建一个数组还是一个对象
+  const newObj: any = obj instanceof Array ? [] : {};
+  for (const key in obj) {
+    // 遍历obj,并且判断是obj的属性才拷贝
+    if (obj.hasOwnProperty(key)) {
+      // 判断属性值的类型，如果是对象递归调用深拷贝
+      newObj[key] = typeof obj[key] === 'object' ? deepCopy(obj[key]) : obj[key];
+    }
+  }
+  return newObj;
+}
+
 export {
   escapeAssertion,
   removeComments,
