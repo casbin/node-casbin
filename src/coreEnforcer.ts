e69b014251d15785a32e1eca56d89e2abb27dce0
Shivansh Yadav <yadavshivansh@gmail.com>
2022-02-12 18:48:59 +0530

    fix: *matcher result should be boolean or number* for  KeyGet2

--- src/coreEnforcer.ts
+++ src/coreEnforcer.ts
@@ -457,6 +457,13 @@ export class CoreEnforcer {
               eftRes = result;
             }
             break;
+          case 'string':
+            if (result === '') {
+              eftRes = Effect.Indeterminate;
+            } else {
+              eftRes = Effect.Allow;
+            }
+            break;
           default:
             throw new Error('matcher result should be boolean or number');
         }
