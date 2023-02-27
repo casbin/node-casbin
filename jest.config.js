0f40beed52d5cae9e47ce2da9ebfcab5c52a4038
Zixuan Liu <nodeces@gmail.com>
2023-02-27 13:14:08 +0800

    fix: built-in buffer and upgrade csv-parse (#436)
    Signed-off-by: Zixuan Liu <nodeces@gmail.com>

--- jest.config.js
+++ jest.config.js
@@ -3,5 +3,8 @@ module.exports = {
   transform: {
     '^.+\\.(ts|tsx)$': 'ts-jest',
   },
+  moduleNameMapper: {
+    'csv-parse': '<rootDir>/node_modules/csv-parse/dist/cjs/sync.cjs',
+  },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json'],
 };
